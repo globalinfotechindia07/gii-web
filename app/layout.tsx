96037fffeb5c4be343317010b3d343eab816a9e0
Mansi <mansibsharma10@gmail.com>
2026-03-10 17:12:29 +0530

    add out folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import "./globals.css";
 
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
