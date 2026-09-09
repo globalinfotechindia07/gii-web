7729f69451a3dc5dca2c311e3ec025b15b467963
globalinfotechindia07 <globalinfotechindia07@gmail.com>
2026-09-09 11:01:45 +0530

    Update HeroSection.jsx

--- app/hero-section/HeroSection.jsx
+++ app/hero-section/HeroSection.jsx
@@ -87,7 +87,7 @@ export default function HeroSection() {
             Next-Gen
           </span>
           <br />
-          Digital Experience
+          Digital Experienced
         </h1>
 
         {/* Description */}
